7411de2d7fd35ba15743424f3e6a9da59b9fcb82
lorenzo-carpentieri <lorenzocarpentieri34@gmail.com>
2026-01-17 18:58:52 +0000

    Added first implementation of all2all for oneCCL without log info.

---
+++ energy/src/intel/a2a_occl.cpp
@@ -0,0 +1,163 @@
+
+// You can use template paramter for specilize the funcition that run the collective with different data types
+//TODO: Test multi nodo per vedere se cambia qualcosa dal punto di vista delle performance e dell'energy
+#include <mpi.h>
+#include "../utils/occl_utils.hpp"
+#include "../../include/utils/ccl_data_types.hpp"  
+#include <oneapi/ccl.hpp> // oneCCL main header
+
+#include <chrono>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include "../../energy-profiler/include/profiler/power_profiler.hpp"
+#include <vector>
+
+#define MAX_RUN 5
+#define WARM_UP_RUN 5
+#define TIME_TO_ACHIEVE_MS 2000
+#define POWER_SAMPLING_RATE_MS 5
+#define MAX_BUF 100
+#define MESSAGE_SIZE_FACTOR 4
+
+namespace log = common::logger; // define logger namespace abbreviation
+namespace data_types = common::utils::data_types; // define data_types namespace abbreviation
+
+using Comm = data_types::CommWrapper<>::type;  // automaticall select ccl::communicator at compile time accroding to the defined macro USE_ONECCL
+
+template<typename T>
+void run(intel::utils::OneCCLContext& ctx, std::string& power_log_path){ 
+
+    sycl::queue q = ctx.q; // sycl queue
+    ccl::stream stream =std::move(*ctx.stream); // ccl stream
+    Comm comm = std::move(*ctx.comm);
+    log::Logger csv_log = std::move(*ctx.logger);
+
+    int rank = ctx.global_rank;
+    int numGPUs = ctx.local_rank_size;
+    
+    constexpr size_t ONE_GB = 1024 * 1024 * 1024;
+    size_t *buff_size_byte = (size_t *) malloc(sizeof(size_t) * MAX_BUF);
+    size_t num_elements=1;
+
+    int i=0;
+
+    while(num_elements * sizeof(T) <= ONE_GB ){
+        buff_size_byte[i] = num_elements * sizeof(T);
+        num_elements *= MESSAGE_SIZE_FACTOR;
+        i++;
+    }
+
+    const int num_iters = i; // define the number of different message sizes to test in a single benchmark run
+
+    // allocate device pointer
+    T *d_sendbuf = sycl::malloc_device<T>(buff_size_byte[num_iters - 1], q); 
+    T *d_recvbuf = sycl::malloc_device<T>(buff_size_byte[num_iters - 1], q); 
+
+    /* init device side buffer */
+    sycl::event init_event = q.submit([&](auto& h) {
+        h.parallel_for(buff_size_byte[num_iters - 1] / sizeof(T), [=](auto id) {
+            d_sendbuf[id] = rank + id + 1;
+            d_recvbuf[id] = -1;
+        });
+    });
+
+    // create dependencies vector: can be used in the collective to ensure that consecutive call to the collective are exectude in order
+    std::vector<ccl::event> deps;
+    auto attr = ccl::create_operation_attr<ccl::alltoall_attr>();
+
+
+    // allocate host pointer
+    T *h_sendbuf = (T *)malloc(buff_size_byte[num_iters - 1]);
+    T *h_recvbuf = (T *)malloc(buff_size_byte[num_iters - 1]);
+
+
+    for (int i = 0; i < WARM_UP_RUN; i++) {
+        q.memcpy(h_sendbuf, d_sendbuf, buff_size_byte[0]).wait();
+
+        auto start = std::chrono::high_resolution_clock::now();
+        ccl::alltoall(d_sendbuf, d_recvbuf,  buff_size_byte[0] / sizeof(T), comm, stream, attr, deps).wait();
+
+        auto end = std::chrono::high_resolution_clock::now();
+        auto time = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+        
+        q.memcpy(d_sendbuf, h_sendbuf, buff_size_byte[0]).wait();       
+    }
+
+ 
+
+    auto mem_cpy_t_start = std::chrono::high_resolution_clock::now();
+    q.memcpy(h_sendbuf, d_sendbuf, buff_size_byte[num_iters-1]).wait();
+    auto mem_cpy_t_end = std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < num_iters; i++) {
+        int chain_size = 0; //  num of times that a collective is executed 
+        int host_energy_counter=MAX_RUN;
+        for (int run = 0; run < MAX_RUN; run++) {
+            double a2a_time_max = 0;
+            chain_size = 0;
+            //TODO: PowerProfier save power data and corrsipective timestamp into a csv file. We can store the results into a vector of tuple (power,timestamp) 
+            // then parse the tuple by removing all the consecutive element for which the power remain the same so that the resulting 
+            // std::string power_file = power_log_path + "/ar_nccl_" + std::to_string(buff_size_byte[i]) + "B"+"_rank"+ std::to_string(rank) + ".pow";
+            std::string power_file = power_log_path + "_" + std::to_string(buff_size_byte[i]) + "B"+"_rank"+ std::to_string(rank) + ".pow";
+            PowerProfiler powerProf(rank % numGPUs, POWER_SAMPLING_RATE_MS, power_file);
+            powerProf.start();
+            int64_t a2a_time_per_rank=0; // Store the time spent for each rank to complete the collective
+            while (a2a_time_max < (TIME_TO_ACHIEVE_MS * 1000)) {
+                auto start_s = std::chrono::high_resolution_clock::now();
+                
+                ccl::alltoall(d_sendbuf, d_recvbuf, (buff_size_byte[i] / sizeof(T)) , comm, stream, attr, deps).wait();
+
+                auto end_s = std::chrono::high_resolution_clock::now();
+                // Time to solution of the current rank
+                int64_t single_run_us = std::chrono::duration_cast<std::chrono::microseconds>(end_s - start_s).count();
+                a2a_time_per_rank+=single_run_us;
+
+                // Time to solution to complete the collective 
+                MPI_Allreduce(MPI_IN_PLACE, &single_run_us, 1, MPI_FLOAT, MPI_MAX, MPI_COMM_WORLD); // Max time to solution
+                a2a_time_max += single_run_us; 
+                chain_size++;
+            }
+           
+            powerProf.stop();
+            
+            double dev_energy_mj = powerProf.get_device_energy(); //device energy in mj for one collective run for the current rank
+            
+            //TODO: add support for host energy
+            // double host_energy_mj = powerProf.get_host_energy() / static_cast<double>(chain_size); //host energy in mj for one collective run        
+        }
+    }
+   
+    q.memcpy(d_recvbuf, h_recvbuf, buff_size_byte[num_iters-1]).wait();
+
+    sycl::free(d_sendbuf, q);
+    sycl::free(d_recvbuf, q);
+    free(h_sendbuf);
+    free(h_recvbuf);
+}
+
+int main(int argc, char *argv[]) {
+    int rank, size;
+    std::string power_log_path;
+    std::string csv_log_path;
+    
+    if (argc != 3)
+        return -1;
+    else{   
+        power_log_path = argv[1];
+        csv_log_path = argv[2];
+    }
+
+    intel::utils::OneCCLContext ctx = intel::utils::init_oneccl(csv_log_path,"a2a", intel::utils::GPUMode::Composite); // initialize oneCCL and MPI
+
+    // Run with different data type
+    // run<uint8_t>(comm, rank, numGPUs, power_log_path, csv_log_path);
+    // run<int>(comm, rank, numGPUs, power_log_path, csv_log_path);
+    run<float>(ctx, power_log_path);
+    // run<double>(comm, rank, numGPUs, power_log_path, csv_log_path);
+
+    
+    MPI_Finalize();
+
+    return 0;
+}
