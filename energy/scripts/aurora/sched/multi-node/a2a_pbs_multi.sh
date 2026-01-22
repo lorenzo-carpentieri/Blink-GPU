27f9457c7382101bb16cef1dcbaba0057ebdcd9b
lorenzo-carpentieri <lorenzocarpentieri34@gmail.com>
2026-01-22 13:49:03 +0000

    Fixed PBS script wrong resource allcoation for multi node jobs.

--- energy/scripts/aurora/sched/multi-node/a2a_pbs_multi.sh
+++ energy/scripts/aurora/sched/multi-node/a2a_pbs_multi.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #PBS -N a2a_occl_multinode
 #PBS -A  EnergyOpt_PhaseFreq
-#PBS -l select=2:ncpus=2:ngpus=12
-#PBS -l place=excl
+#PBS -l select=2
 #PBS -l walltime=00:10:00
 #PBS -l filesystems=home
 #PBS -o /home/lcarpent/energy-workspace/Blink-GPU/energy/logs/pbs-out/output_a2a_occl.txt
@@ -23,6 +22,29 @@ export CCL_LOG_LEVEL=error
 export CCL_ALLTOALLV_MONOLITHIC_KERNEL=${SCALEUP_MODE} # SCALE_UP mode can be 0 (use copy engines) or 1 (use GPU compute kernel to transfer data across GPUs). 
 export CCL_ALLTOALL_SCALEOUT=${ALG_NAME} # Not used in single node but set for consistency.
 
+# MPI example w/ 12 MPI ranks per node each with access to single GPU tile
+NNODES=`wc -l < $PBS_NODEFILE`
+
+NRANKS_PER_NODE=12 # use all gpus onthe node
+NDEPTH=1
+NTHREADS=1
+
+NTOTRANKS=$(( NNODES * NRANKS_PER_NODE ))
+echo "NUM_OF_NODES= ${NNODES} TOTAL_NUM_RANKS= ${NTOTRANKS} RANKS_PER_NODE= ${NRANKS_PER_NODE} THREADS_PER_RANK= ${NTHREADS}"
+
+MPI_ARG="-n ${NTOTRANKS} --ppn ${NRANKS_PER_NODE} --depth=${NDEPTH} --cpu-bind depth "
+
+AFFINITY=""
+AFFINITY="/home/lcarpent/energy-workspace/Blink-GPU/energy/scripts/aurora/env/gpu_affinity.sh"
+
+
+# mpiexec -n 12 -ppn 12  ${EXE_PATH} ${power_file} ${csv_file}
+
+
+
 power_file="${POWER_LOG_DIR}/multi-node/occl/${COLLECTIVE}_${SCALEUP_MODE}_${ALG_NAME}_pow.csv"
 csv_file="${CSV_LOG_DIR}/multi-node/occl/${COLLECTIVE}_${SCALEUP_MODE}_${ALG_NAME}.csv"
-mpiexec -n 24 -ppn 12  ${EXE_PATH} ${power_file} ${csv_file}
+
+COMMAND="mpiexec ${MPI_ARG} ${EXE_PATH} ${power_file} ${csv_file}"
+
+$COMMAND
--- energy/scripts/aurora/sched/single-node/a2a_pbs_single.sh
+++ energy/scripts/aurora/sched/single-node/a2a_pbs_single.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #PBS -N a2a_occl
 #PBS -A  EnergyOpt_PhaseFreq
-#PBS -l select=1:ncpus=1:ngpus=3
+#PBS -l select=1
 #PBS -l walltime=00:10:00
 #PBS -l filesystems=home
 #PBS -o /home/lcarpent/energy-workspace/Blink-GPU/energy/logs/pbs-out/output_a2a_occl.txt
 #PBS -e /home/lcarpent/energy-workspace/Blink-GPU/energy/logs/pbs-out/error_a2a_occl.txt
-#PBS -q prod
+#PBS -q debug
 
 echo "Executing a2a ..."  # Output: app_name
 echo "Unique nodes allocated:"
@@ -18,10 +18,29 @@ export ONEAPI_DEVICE_SELECTOR=level_zero:gpu
 export ZE_FLAT_DEVICE_HIERARCHY=FLAT
 export CCL_LOG_LEVEL=error
 
+# MPI example w/ 12 MPI ranks per node each with access to single GPU tile
+NNODES=`wc -l < $PBS_NODEFILE`
+
+NRANKS_PER_NODE=12
+NDEPTH=1
+NTHREADS=1
+
+NTOTRANKS=$(( NNODES * NRANKS_PER_NODE ))
+echo "NUM_OF_NODES= ${NNODES} TOTAL_NUM_RANKS= ${NTOTRANKS} RANKS_PER_NODE= ${NRANKS_PER_NODE} THREADS_PER_RANK= ${NTHREADS}"
+
+MPI_ARG="-n ${NTOTRANKS} --ppn ${NRANKS_PER_NODE} --depth=${NDEPTH} --cpu-bind depth "
+
+AFFINITY=""
+AFFINITY="/home/lcarpent/energy-workspace/Blink-GPU/energy/scripts/aurora/env/gpu_affinity.sh"
+
+
 # Set scaleup and scaleout mode for single node. Scaleout mode is not used in single node.
 export CCL_ALLTOALLV_MONOLITHIC_KERNEL=${SCALEUP_MODE} # SCALE_UP mode can be 0 (use copy engines) or 1 (use GPU compute kernel to transfer data across GPUs). 
 # export CCL_ALLTOALL_SCALEOUT="none" # Not used in single node but set for consistency.
 
 power_file="${POWER_LOG_DIR}/single-node/occl/${COLLECTIVE}_${SCALEUP_MODE}_${ALG_NAME}_pow.csv"
 csv_file="${CSV_LOG_DIR}/single-node/occl/${COLLECTIVE}_${SCALEUP_MODE}_${ALG_NAME}.csv"
-mpiexec -n 3 -ppn 3  ${EXE_PATH} ${power_file} ${csv_file}
+
+COMMAND="mpiexec ${MPI_ARG} ${EXE_PATH} ${power_file} ${csv_file}"
+# mpiexec -n 12 -ppn 12  ${EXE_PATH} ${power_file} ${csv_file}
+$COMMAND
