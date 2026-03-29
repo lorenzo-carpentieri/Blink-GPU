ed70329975e0abbf4952c04b64dc57ab8b275440
lorenzo-carpentieri <lorenzocarpentieri34@gmail.com>
2026-03-29 10:01:29 +0000

    Added script to compile microbenchmark on Aurora.

---
+++ energy/scripts/aurora/compile/comile.sh
@@ -0,0 +1,17 @@
+
+PROJ_DIR=$1
+BUILD_DIR=${PROJ_DIR}/build
+SOURCE_DIR=${PROJ_DIR}
+
+source ${SOURCE_DIR}/scripts/aurora/env/occl_set_env.sh
+cmake -S ${SOURCE_DIR} -B ${BUILD_DIR} \
+    -DENABLE_ONECCL=ON \
+    -DCMAKE_CXX_COMPILER=icpx \
+    -DCMAKE_CXX_FLAGS="-fsycl -fsycl-targets=intel_gpu_pvc" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DUSE_NVML=OFF \
+    -DUSE_LEVEL_ZERO=ON \
+    -DUSE_RAPL=OFF \
+    -DCMAKE_INSTALL_PREFIX=${BUILD_DIR} 
+
+cmake --build ${BUILD_DIR} -j --target install
