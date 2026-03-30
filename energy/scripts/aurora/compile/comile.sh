7c60c6111315f1127628ea411f6cbbffc82ed6a4
lorenzo-carpentieri <lorenzocarpentieri34@gmail.com>
2026-03-30 14:54:33 +0000

    Fixed pthread error.

--- energy/scripts/aurora/compile/comile.sh
+++ energy/scripts/aurora/compile/comile.sh
@@ -7,7 +7,7 @@ source ${SOURCE_DIR}/scripts/aurora/env/occl_set_env.sh
 cmake -S ${SOURCE_DIR} -B ${BUILD_DIR} \
     -DENABLE_ONECCL=ON \
     -DCMAKE_CXX_COMPILER=icpx \
-    -DCMAKE_CXX_FLAGS="-fsycl -fsycl-targets=intel_gpu_pvc" \
+    -DCMAKE_CXX_FLAGS="-fsycl -fsycl-targets=intel_gpu_pvc -lpthread" \
     -DCMAKE_BUILD_TYPE=Release \
     -DUSE_NVML=OFF \
     -DUSE_LEVEL_ZERO=ON \
