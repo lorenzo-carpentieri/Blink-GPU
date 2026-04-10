f1f47a92a40d5f85b21ad6df1c4d849cd33b6f4f
lorenzo-carpentieri <lorenzocarpentieri34@gmail.com>
2026-04-10 13:09:47 +0000

    Added bash script used to generate singl node PBS script

--- energy/scripts/aurora/pbs/run_coll.sh
+++ energy/scripts/aurora/pbs/run_coll.sh
@@ -7,7 +7,7 @@
 #PBS -o /home/lcarpent/energy-workspace/Blink-GPU/energy/logs/pbs-out/a2a_n1.out
 #PBS -e /home/lcarpent/energy-workspace/Blink-GPU/energy/logs/pbs-out/a2a_n1.err
 #PBS -q debug
-
+#PBS -l place=excl
 echo "Executing single node ${COLLECTIVE} ..."  # Output: app_name
 echo "Unique nodes allocated:"
 sort -u $PBS_NODEFILE
@@ -16,7 +16,7 @@ source ${SET_ENV_PATH}
 export  ZES_ENABLE_SYSMAN=1
 export ONEAPI_DEVICE_SELECTOR=level_zero:gpu
 export ZE_FLAT_DEVICE_HIERARCHY=FLAT
-# export CCL_LOG_LEVEL=error
+export CCL_LOG_LEVEL=info
 
 NNODES=`wc -l < $PBS_NODEFILE`
 
@@ -29,16 +29,27 @@ echo "NUM_OF_NODES= ${NNODES} TOTAL_NUM_RANKS= ${NTOTRANKS} RANKS_PER_NODE= ${NR
 
 MPI_ARG="-n ${NTOTRANKS} --ppn ${NRANKS_PER_NODE} --depth=${NDEPTH} --cpu-bind depth "
 
-export CCL_ALLTOALLV_MONOLITHIC_KERNEL=${SCALEUP_MODE} # alltoall
-export CCL_REDUCE_SCATTER_MONOLITHIC_KERNEL=${SCALEUP_MODE} # allreduce
 
-# TODO: Add export for scaleout mode
+############## VTUNE ARGUMENTS ##############
+# # Vtune suggest to use aps for MPI program analysis
+# VTUNE_LOG_DIR="/home/lcarpent/energy-workspace/Blink-GPU/energy/logs/vtune/"
+# # VTUNE_ARG="vtune -collect gpu-hotspots -r ${VTUNE_LOG_DIR}"
+# VTUNE_ARG="vtune -collect gpu-offload -r ${VTUNE_LOG_DIR}"
 
-echo "Scaleup mode set to: ${CCL_ALLTOALLV_MONOLITHIC_KERNEL}" 
+# export CCL_ITT_LEVEL=1
 
-mkdir -p ${CSV_LOG_DIR}/single-node/${COLLECTIVE}/
-csv_file="${CSV_LOG_DIR}/single-node/${COLLECTIVE}/${COLLECTIVE}_n${NNODES}_scaleup${SCALEUP_MODE}_scaleout${SCALEOUT_MODE}.csv"
+# Note Vtune: is not able to detect MPI info and it suggest to use APS
+############## APS ARGUMENTS ###############
 
+if [ "$NNODES" -gt 1 ]; then
+    mkdir -p ${CSV_LOG_DIR}/multi-node/${COLLECTIVE}/
+    csv_file="${CSV_LOG_DIR}/multi-node/${COLLECTIVE}/${COLLECTIVE}_n${NNODES}_scaleup${SCALEUP_MODE}_scaleout${SCALEOUT_MODE}_mnic${NIC_MODE}.csv"
+else
+    mkdir -p ${CSV_LOG_DIR}/single-node/${COLLECTIVE}/
+    csv_file="${CSV_LOG_DIR}/single-node/${COLLECTIVE}/${COLLECTIVE}_n${NNODES}_scaleup${SCALEUP_MODE}_scaleoutDefault_mnicDefault.csv"
+fi
 
-COMMAND="mpiexec ${MPI_ARG} ${EXE_PATH} ${csv_file}"
+# COMMAND="mpiexec ${MPI_ARG} ${VTUNE_ARG} ${EXE_PATH} ${csv_file}"
+COMMAND="mpiexec ${MPI_ARG}  ${EXE_PATH} ${csv_file}"
+
 $COMMAND
